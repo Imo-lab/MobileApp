4054e857d0881a8b0deafc323536c5ec8728da28
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-06-19 00:07:40 +0700

    feat: editing API

--- app/src/main/java/com/example/hahh/RetrofitClient.kt
+++ app/src/main/java/com/example/hahh/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbxy3mX1V7gQprYF5HCWhif4MR-fnuwzmxVTzG61dY-IRA-tWR2Tps-JhvmCjCXnbSb5gg/exec/"
+    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbymbnJFsLBAq76UZv7cq8fG2rliTpIYmMD93oU8ITmS8pcVWkJ6KhyY62OswYuc5nlwEA/"
 
     val instance: APIService by lazy {
         val retrofit = Retrofit.Builder()
